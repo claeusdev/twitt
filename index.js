2649f8d6b2f429c3b9115e36570da0dc8504faf2
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-18 03:05:17 +0000

    setup models

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post")
+const User = require("./models/User");
+
 const { MongoURL } = require("./config");
 const typeDefs = gql`
+    type Post{
+        id: ID!,
+        body: String!,
+        createdAt: String!,
+        username: String!
+    }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello world"
+    getPosts: async () => {
+        try {
+            const posts = await Post.find()
+            return posts
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
   }
 };
 
