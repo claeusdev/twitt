2db07f1c9d639fdff2e53711e6693676aca4ecc7
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-18 02:51:43 +0000

    connect to DB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
+const { MongoURL } = require("./config");
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -18,10 +20,20 @@ const server = new ApolloServer({
   resolvers
 });
 
-server
-  .listen({
-    port: 5000
+mongoose
+  .connect(MongoURL, { useNewUrlParser: true })
+  .then(() => {
+    return server
+      .listen({
+        port: 5000
+      })
+      .then(res => {
+        console.log(`Server running at ${res.url}`);
+      })
+      .catch(err => {
+        console.log(error);
+      });
   })
-  .then(res => {
-    console.log(`Server running at ${res.url}`);
+  .catch(err => {
+    console.log(err);
   });
