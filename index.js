0646c45e068286e41e81ff85c79cdc4990abe055
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-18 03:16:17 +0000

    setup resolvers n typedefs

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
-const Post = require("./models/Post")
-const User = require("./models/User");
-
+const typeDefs = require("./graphql/typeDefs")
+const resolvers = require('./graphql/resolvers')
 const { MongoURL } = require("./config");
-const typeDefs = gql`
-    type Post{
-        id: ID!,
-        body: String!,
-        createdAt: String!,
-        username: String!
-    }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    getPosts: async () => {
-        try {
-            const posts = await Post.find()
-            return posts
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
-  }
-};
 
 const server = new ApolloServer({
   typeDefs,
